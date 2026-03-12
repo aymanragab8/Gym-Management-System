726432ce168c6c6e29662836c44c20f6636a5c1a
Ayman Ragab <114214010+aymanragab8@users.noreply.github.com>
2026-03-12 13:40:56 +0100

    Add : Member Infrastructure Layer

---
+++ GymSystem.Infrastructure/Data/Configurations/MemberConfigurations.cs
@@ -0,0 +1,37 @@
+﻿using GymSystem.Domain.Entities;
+using GymSystem.Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymSystem.Infrastructure.Data.Configurations
+{
+    public class MemberConfigurations : IEntityTypeConfiguration<Member>
+    {
+        public void Configure(EntityTypeBuilder<Member> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.FullName).IsRequired().HasMaxLength(100);
+            builder.HasIndex(m => m.Email).IsUnique();
+            builder.Property(m => m.Email).IsRequired().HasMaxLength(150);
+            builder.Property(m => m.PhoneNumber).IsRequired().HasMaxLength(15);
+            builder.Property(m => m.DateOfBirth).IsRequired();
+            builder.Property(m => m.UpdatedAt).IsRequired(false);
+            builder.HasQueryFilter(m => !m.IsDeleted);
+            builder.HasIndex(m => m.Email)
+                   .IsUnique()
+                   .HasDatabaseName("UQ_Member_Email");
+
+            builder.HasOne<ApplicationUser>()
+                   .WithMany()
+                   .HasForeignKey(m => m.ApplicationUserId);
+
+            builder.HasOne(m => m.Coach)
+                   .WithMany(m => m.CoachedMembers)
+                   .HasForeignKey(m => m.CoachId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+
+
+        }
+    }
+}
---
+++ GymSystem.Infrastructure/Repositories/MemberRepository.cs
@@ -0,0 +1,42 @@
+﻿using GymSystem.Domain.Entities;
+using GymSystem.Infrastructure.GenericImplementation;
+using GymSystem.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models.Data;
+
+namespace GymSystem.Infrastructure.Repositories
+{
+    public class MemberRepository : GenericRepository<Member>, IMemberRepository
+    {
+        public MemberRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Member>> GetAllAsync(int pageNumber, int pageSize)
+        {
+            pageNumber = pageNumber <= 0 ? 1 : pageNumber;
+            pageSize = pageSize <= 0 ? 10 : pageSize;
+
+            return await _dbContext.Members
+                .OrderBy(m => m.FullName).Include(m => m.Subscriptions)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+        public override async Task<Member?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Members
+                .AsNoTracking()
+                .Include(m => m.Subscriptions)
+                .FirstOrDefaultAsync(m => m.Id == id);
+        }
+        public async Task<Member?> GetByApplicationUserIdAsync(string applicationUserId)
+        {
+            return await _dbContext.Members
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ApplicationUserId == applicationUserId);
+        }
+
+    }
+}
