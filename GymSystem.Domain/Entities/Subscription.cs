74e050d5a7cee827751d6c01790c1e906018d804
Ayman Ragab <114214010+aymanragab8@users.noreply.github.com>
2026-03-13 11:07:48 +0100

    Add : Subscription Entity In Domain Layer

---
+++ GymSystem.Domain/Entities/Subscription.cs
@@ -0,0 +1,15 @@
+﻿using GymSystem.Domain.Enums;
+
+namespace GymSystem.Domain.Entities
+{
+    public class Subscription : BaseEntity
+    {
+        public decimal Price { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public SubscriptionPeriod SubscriptionType { get; set; }
+        public int MemberId { get; set; }
+        public Member? Member { get; set; }
+        public ICollection<Payment>? Payments { get; set; }
+    }
+}
---
+++ GymSystem.Domain/Enums/SubscriptionPeriod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymSystem.Domain.Enums
+{
+    public enum SubscriptionPeriod
+    {
+        Monthly = 0,
+        Quarterly = 1,
+        Yearly = 2
+    }
+}
