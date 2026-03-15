08e09671da906b81636cd381a65d0d7eba37b265
Ayman Ragab <114214010+aymanragab8@users.noreply.github.com>
2026-03-15 14:45:52 +0100

    Add : Infrastructure Dependencies

---
+++ GymSystem.Infrastructure/ModuleInfrastructureDependencies.cs
@@ -0,0 +1,67 @@
+﻿using GymSystem.Application.Abstracts;
+using GymSystem.Application.Interfaces;
+using GymSystem.Infrastructure.Identity;
+using GymSystem.Infrastructure.Interfaces;
+using GymSystem.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WebApplication2.Models.Data;
+
+namespace GymSystem.Infrastructure
+{
+    public static class ModuleInfrastructureDependencies
+    {
+        public static IServiceCollection AddInfrastructureDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+
+            //DI
+            services.AddScoped<IMemberRepository, MemberRepository>();
+            services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+            services.AddScoped<IExerciseRepository, ExerciseRepository>();
+            services.AddScoped<IWorkoutPlanRepository, WorkoutPlanRepository>();
+            services.AddScoped<IWorkoutPlanExerciseRepository, WorkoutPlanExerciseRepository>();
+            services.AddScoped<IAttendanceRepository, AttendanceRepository>();
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
+
+
+
+            // DbContext
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("cs")));
+
+            // Identity
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
+
+            // JWT Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "Bearer";
+                options.DefaultChallengeScheme = "Bearer";
+            })
+            .AddJwtBearer("Bearer", options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["Jwt:IssuerUrl"],
+                    ValidAudience = configuration["Jwt:AudienceUrl"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:SecretKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+
+            return services;
+        }
+    }
+}
