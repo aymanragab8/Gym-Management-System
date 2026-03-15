a6092b5af70a2350164a61677322cb10a9a0bc3a
Ayman Ragab <114214010+aymanragab8@users.noreply.github.com>
2026-03-15 14:54:38 +0100

    Add : Subscription, Attendance And Payment Controllers In Api Layer

---
+++ Gym-Management-System/Controllers/AttendanceController.cs
@@ -0,0 +1,50 @@
+﻿using GymSystem.Application.Interfaces;
+using GymSystem.Domain.AppMeteData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Gym_Management_System.Controllers
+{
+    [ApiController]
+    [Authorize]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly IAttendanceService _attendanceService;
+
+        public AttendanceController(IAttendanceService attendanceService)
+        {
+            _attendanceService = attendanceService;
+        }
+
+        [HttpPost(Router.Attendance.CheckIn)]
+        [Authorize(Roles = "Member")]
+        public async Task<IActionResult> CheckIn()
+        {
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = await _attendanceService.CheckIn(currentUserId);
+            return Ok(result);
+        }
+        [HttpGet(Router.Attendance.GetMemberAttendance)]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetMemberAttendance(int memberId)
+        {
+            var attendanceData = await _attendanceService.MemberAttendance(memberId);
+            return Ok(attendanceData);
+        }
+        [HttpGet(Router.Attendance.GetMemberAttendanceByDate)]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetMembersAttendanceByDate(DateTime date)
+        {
+            var attendanceData = await _attendanceService.MemberAttendanceByDate(date);
+            return Ok(attendanceData);
+        }
+        [HttpDelete(Router.Attendance.DeleteMemberAttendance)]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteMemberAttendance(int memberId)
+        {
+            var result = await _attendanceService.DeleteMmberAttendance(memberId);
+            return Ok(result);
+        }
+    }
+}
---
+++ Gym-Management-System/Controllers/PaymentController.cs
@@ -0,0 +1,51 @@
+﻿using GymSystem.Application.Dtos.Payment;
+using GymSystem.Application.Interfaces;
+using GymSystem.Domain.AppMeteData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Gym_Management_System.Controllers
+{
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IPaymentService _ipaymentService;
+
+        public PaymentController(IPaymentService ipaymentService)
+        {
+            _ipaymentService = ipaymentService;
+        }
+        [HttpGet(Router.Payment.GetAllPayments)]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllPayments(int pageNumber = 1, int pageSize = 10)
+        {
+            var payments = await _ipaymentService.GetAllPaymentsAsync(pageNumber, pageSize);
+            return Ok(payments);
+        }
+        [HttpGet(Router.Payment.GetAllMemberPayments)]
+        [Authorize(Roles = "Admin,Member")]
+        public async Task<IActionResult> GetAllMemberPayments(int memberId)
+        {
+            string currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string role = User.FindFirstValue(ClaimTypes.Role);
+            var payments = await _ipaymentService.GetMemberPaymentsAsync(memberId, currentUserId, role);
+            return Ok(payments);
+        }
+        [HttpGet(Router.Payment.GetPaymentById)]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetPaymentById(int paymentId)
+        {
+            var payment = await _ipaymentService.GetPaymentByIdAsync(paymentId);
+            return Ok(payment);
+        }
+        [HttpPost(Router.Payment.ProcessPayment)]
+        [Authorize(Roles = "Member")]
+        public async Task<IActionResult> ProcessPayment(CreatePaymentDto dto)
+        {
+            string currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = await _ipaymentService.ProcessPaymentAsync(currentUserId, dto);
+            return Ok(result);
+        }
+    }
+}
